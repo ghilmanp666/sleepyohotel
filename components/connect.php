50b0bbf9d2843b19d6951a1ddba94d0586df9d51
team10store <140830642+ghilmanp666@users.noreply.github.com>
2025-10-27 21:54:04 +0700

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -12,4 +12,16 @@ try {
     die("Connection failed: " . $e->getMessage());
 }
 ?>
+<?php
+$conn = new mysqli(
+    getenv('MYSQLHOST'),
+    getenv('MYSQLUSER'),
+    getenv('MYSQLPASSWORD'),
+    getenv('MYSQLDATABASE')
+);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 
