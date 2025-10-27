99e0054fe46e807b247a779d89df1b709c64e7b7
team10store <140830642+ghilmanp666@users.noreply.github.com>
2025-10-27 21:58:09 +0700

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,18 +1,4 @@
 <?php
-$servername = "mysql.railway.internal";   // ganti sesuai plugin MySQL di Railway
-$username   = "root";
-$password   = "IIsKyCqhHSHdZCIRiCNJzdosnQSMRrsS";
-$dbname     = "railway";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully"; // bisa aktifkan untuk uji coba
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-?>
-<?php
 $conn = new mysqli(
     getenv('MYSQLHOST'),
     getenv('MYSQLUSER'),
@@ -24,4 +10,5 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
 
