23fd58577149115331b972d028a8cfd911e78c39
team10store <140830642+ghilmanp666@users.noreply.github.com>
2025-10-27 22:01:33 +0700

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,14 +1,22 @@
 <?php
-$conn = new mysqli(
-    getenv('MYSQLHOST'),
-    getenv('MYSQLUSER'),
-    getenv('MYSQLPASSWORD'),
-    getenv('MYSQLDATABASE')
-);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+   $db_name = 'mysql:host=mysql.railway.internal;dbname=railway';
+   $db_user_name = 'root';
+   $db_user_pass = 'IIsKyCqhHSHdZCIRiCNJzdosnQSMRrsS';
+
+   $conn = new PDO($db_name, $db_user_name, $db_user_pass);
+
+   function create_unique_id(){
+      $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+      $rand = array();
+      $length = strlen($str) - 1;
+
+      for($i = 0; $i < 20; $i++){
+         $n = mt_rand(0, $length);
+         $rand[] = $str[$n];
+      }
+      return implode($rand);
+   }
+
 ?>
-
 
